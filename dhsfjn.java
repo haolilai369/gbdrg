8c6ac1af1452828db4593f5c35bd424acd82afc1
wuqing <140866570+haolilai369@users.noreply.github.com>
2024-04-13 17:15:48 +0800

    Create dhsfjn.java

---
+++ dhsfjn.java
@@ -0,0 +1,50 @@
+public class Test {
+ 
+   public static void main(String args[]){
+      int x = 30;
+      int y = 10;
+ 
+      if( x == 30 ){
+         if( y == 10 ){
+             System.out.print("X = 30 and Y = 10");
+          }
+       }
+    }
+}
+switch(expression){
+    case value :
+       //语句
+       break; //可选
+    case value :
+       //语句
+       break; //可选
+    //你可以有任意数量的case语句
+    default : //可选
+       //语句
+}
+public class Test {
+   public static void main(String args[]){
+      //char grade = args[0].charAt(0);
+      char grade = 'C';
+ 
+      switch(grade)
+      {
+         case 'A' :
+            System.out.println("优秀"); 
+            break;
+         case 'B' :
+         case 'C' :
+            System.out.println("良好");
+            break;
+         case 'D' :
+            System.out.println("及格");
+            break;
+         case 'F' :
+            System.out.println("你需要再努力努力");
+            break;
+         default :
+            System.out.println("未知等级");
+      }
+      System.out.println("你的等级是 " + grade);
+   }
+}
